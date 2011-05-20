550589b7341e74b6b290a51ab171329902c9d279
kBoussy <the_chosen_dragon@hotmail.com>
2011-05-20 14:43:33 +0200

    Guizmed getDose toegevoegd

--- GuizMed/apps/backend/modules/medicijnbeheer/templates/getmednameSuccess.php
+++ GuizMed/apps/backend/modules/medicijnbeheer/templates/getmednameSuccess.php
@@ -1,6 +1,7 @@
 {
    "medicine":{
 	"id":"<?php echo $med->getMedBaseId()->getMedBaseId(); ?>",
+	"dose":"<?php echo $med->getDose(); ?>",
 	"name":"<?php echo $med->getMedBaseId()->getSpeciality(); ?>"
 
    }
